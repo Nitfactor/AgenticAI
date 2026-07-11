dd62068bd1e6f89cc5b0b384bff0a127bd70dce2
Akshat Singh <nitfactor@gmail.com>
2026-07-11 15:44:06 +0530

    main

--- rag/index.py
+++ rag/index.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 pdf_path = Path(__file__).parent / "Bloomberg.pdf"
 
@@ -8,3 +9,11 @@ pdf_path = Path(__file__).parent / "Bloomberg.pdf"
 loader = PyPDFLoader(file_path=pdf_path)
 documents = loader.load()
 print(documents[2])
+
+# Split the documents into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = text_splitter.split_documents(documents)
+
+# Vector embedding for these chunks
+
+
